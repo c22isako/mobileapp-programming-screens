55853bdf3f710963d84f1a7f47db17f3c2189220
c22isako <c22isako@student.his.se>
2024-04-15 16:47:33 +0200

    Changed name of String variable to Type

--- app/src/main/java/com/example/screens/ExtraActivity.java
+++ app/src/main/java/com/example/screens/ExtraActivity.java
@@ -1,14 +1,9 @@
 package com.example.screens;
 
-import static android.content.Intent.getIntent;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.w3c.dom.Text;
 
 
 public class ExtraActivity extends AppCompatActivity {
@@ -20,14 +15,12 @@ public class ExtraActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            String name = extras.getString("Potato");
+            String type = extras.getString("Potato");
             int number = extras.getInt("seven");
 
-            //Log.d("Tomat", name);
-
             TextView ExtrasTextView = findViewById(R.id.ExtrasTextView);
-            ExtrasTextView.setText("Potatos are great. ");
-            // Do something with the name and number
+            ExtrasTextView.setText("Potatos are great. Especially the type " + type + ". And Atleast " + number + " Each meal.");
+
         }
     }
 }
